90d2e5e06c8c9ca411b7f99e5e9317cb804f54b6
Uwe Dauernheim <uwe@klarna.com>
2011-04-01 11:41:20 +0200

    Fix lists module declaration

--- src/ehf_lists.erl
+++ src/ehf_lists.erl
@@ -1,7 +1,7 @@
 %%% @doc Helper functions.
 %%% @author Uwe Dauernheim <uwe@dauernheim.net>
 %%% @author Programming Erlang - The Pragmatic Bookshelf
--module(ehf:lists).
+-module(ehf_lists).
 
 -author("Uwe Dauernheim <uwe@dauernheim.net>").
 -author("Programming Erlang - The Pragmatic Bookshelf").

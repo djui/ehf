8c52e946b7102f212d7488a03253436b7809ffa8
Uwe Dauernheim <uwe@klarna.com>
2011-09-30 08:23:02 +0200

    Add port version of cmd

--- src/tulib_os.erl
+++ src/tulib_os.erl
@@ -7,25 +7,44 @@
 -export([ cmd/1
         , cmd/2
         , cmd2/1
+        , cmd2/2
+        , cmd3/1
         , exit/1
         , exit/2
         ]).
 
 -define(NL, io_lib:format("~n")).
 
+%% @doc Running an external command as port along with the return code.
+cmd(Cmd) -> cmd(Cmd, []).
+
+cmd(Cmd, Args) ->
+  Port = erlang:open_port({spawn_executable, Cmd}, [exit_status]),
+  try os_cmd_loop(Port, [])
+  catch _C:R -> {error, R}
+  after erlang:close_port(Port)
+  end.
+
+cmd_loop(Port, Data) ->
+  receive
+    {Port, {data, Data}}          -> os_cmd_loop(Port, Data ++ NewData);
+    {Port, {exit_status, Status}} -> {ok, Status, Data};
+    {'EXIT', Port, Reason}        -> {error, Reason}
+  end.
+
 %% @doc Executes an OS command and returns the return code along with the
 %% standard output text.
-cmd(Str) -> cmd(Str, false).
+cmd2(Str) -> cmd2(Str, false).
 
 %% @doc Executes an OS command and returns the return code along with the
 %% standard output text. If split is true, the return output text is split into
 %% lines.
-cmd(Str, Split) when is_boolean(Split) ->
-  Output = os:cmd(Str ++ " ; echo $?"),
-  Lines = string:tokens(Output, ?NL),
+cmd2(Str, Split) when is_boolean(Split) ->
+  Output         = os:cmd(Str ++ " ; echo $?"),
+  Lines          = string:tokens(Output, ?NL),
   [Code|ROutput] = lists:reverse(Lines),
-  {Code2, _} = string:to_integer(Code),
-  RROutput = lists:reverse(ROutput),
+  {Code2, _}     = string:to_integer(Code),
+  RROutput       = lists:reverse(ROutput),
   case Split of
     true  -> {Code2, RROutput};
     false -> {Code2, string:join(RROutput, ?NL)}
@@ -34,7 +53,7 @@ cmd(Str, Split) when is_boolean(Split) ->
 %% @doc Executes an OS command and returns the return code along with the
 %% standard output text. If split is true, the return output text is split into
 %% lines.
-cmd2(Str) -> %% Note: This can only be applied on idempotent operations!
+cmd3(Str) -> %% Note: This can only be applied on idempotent operations!
   Code0 = os:cmd(Str ++ " > /dev/null 2>&1 ; echo $?"),
   {Code, _} = string:to_integer(Code0),
   {Code, os:cmd(Str)}.

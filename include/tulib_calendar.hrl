c6bca1739a8019a6ae514b6d22306e42d6f781b2
Uwe Dauernheim <uwe@klarna.com>
2011-10-15 21:12:40 +0200

    Add calendar functions

---
+++ include/tulib_calendar.hrl
@@ -0,0 +1,6 @@
+-define(A_YEAR,  31536000). %% 365*24*60*60
+-define(A_MONTH,  2592000). %%  30*24*60*60
+-define(A_WEEK,    604800). %%   7*24*60*60
+-define(A_DAY,      86400). %%     24*60*60
+-define(A_HOUR,      3600). %%        60*60
+-define(A_MIN,         60). %%          ...
---
+++ src/tulib_calendar.erl
@@ -0,0 +1,24 @@
+%%% @doc Helper functions.
+%%% @author Uwe Dauernheim <uwe@dauernheim.net>
+-module(tulib_calendar).
+
+-author("Uwe Dauernheim <uwe@dauernheim.net>").
+
+-export([ unix_ts/1
+        , unix_ts/0
+        , unix_ts_to_datetime/1
+        ]).
+
+%% @doc Convert an erlang:now/0 timestamp into an unix timestamp.
+unix_ts() -> unix_ts(now()).
+
+unix_ts({MegaSecs, Secs, _MicroSecs}) -> MegaSecs*1000000 + Secs;
+unix_ts({_Date, _Time}=DateTime) ->
+  Base = calendar:datetime_to_gregorian_seconds({{1970,1,1}, {0,0,0}}),
+  Secs = calendar:datetime_to_gregorian_seconds(DateTime),
+  Secs - Base.
+
+unix_ts_to_datetime(TS) when is_integer(TS) ->
+  Base = calendar:datetime_to_gregorian_seconds({{1970,1,1}, {0,0,0}}),
+  Secs = Base + TS,
+  calendar:gregorian_seconds_to_datetime(Secs).

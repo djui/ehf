43cb31cb7330844071a9a897587753c163999362
Uwe Dauernheim <uwe@klarna.com>
2011-10-20 14:39:01 +0200

    Update tulib_types to have a type checker.

--- src/tulib_types.erl
+++ src/tulib_types.erl
@@ -4,23 +4,86 @@
 
 -author("Uwe Dauernheim <uwe@dauernheim.net>").
 
--export([ complies/2
+-export([ check/2
         ]).
 
 %%% Code =======================================================================
-complies(Value, {Module, Name}) ->
-  Types = extract_types(Module),
-  Type  = proplists:get_value(Name, Types),
-  I = erl_types:t_from_form(Type),
-  T = erl_types:t_from_term(Value),
+%% @doc Runs type-check on data structure against remote/local type definition.
+check(V, {M,N}) -> check(V, M, N);
+check(V, MN) when is_atom(MN) ->
+  case string:tokens(atom_to_list(MN), ":") of
+    [Mod, Name] -> check(V, list_to_atom(Mod), list_to_atom(Name));
+    [Name]      -> check(V, ?MODULE,           list_to_atom(Name))
+  end.
+
+check(Value, TModule, TName) ->
+  Types = from_module(TModule),
+  Type  = resolve(TName, Types),
+  I     = from_form(Type),
+  T     = from_term(Value),
   erl_types:t_is_instance(T, I).
 
 %%% Internals ------------------------------------------------------------------
-extract_types(M) ->
-  {tree, form_list, _, AS} = tulib_beam_lib:ast(M),
-  TypeFun = fun({attribute, _, type, {Name, Form, []}}, Acc) ->
-                [{Name, Form}|Acc];
-               (_, Acc) ->
-                Acc
-            end,
-  lists:foldl(TypeFun, [], AS).
+resolve(Name, Types) -> traverse(form(Name, Types), Types).
+
+traverse({type,Line,Type,SubTypes}, Types) ->
+  case lists:member(Type, builtin_types()) of
+    true  -> ResTypes = lists:map(fun(T) -> traverse(T, Types) end, SubTypes),
+             {type,Line,Type,ResTypes};
+    false -> resolve(Type, Types)
+  end;
+traverse({_BuiltinType,_Line,_Value}=TypeForm, _Types) -> TypeForm.
+
+from_module(Module) ->
+  {ok,AC}    = dialyzer_utils:get_abstract_code_from_beam(code:which(Module)),
+  {ok,Types} = dialyzer_utils:get_record_and_type_info(AC),
+  Types.
+
+from_form(Type) -> catch erl_types:t_from_form(Type).
+
+from_term(Term) -> erl_types:t_from_term(Term).
+
+form(Name, Types) -> {_,TypeForm,[]} = dict:fetch({type,Name}, Types), TypeForm.
+
+%% @doc FIXME This list is probably to greedy.
+builtin_types() ->
+  [ any
+  , atom
+  , binary
+  , boolean
+  , byte
+  , char
+  , float
+  , 'fun'
+  , function
+  , identifier
+  , integer
+  , iolist
+  , list
+  , matchstate
+  , maybe_improper_list
+  , module
+  , mfa
+  , neg_integer
+  , node
+  , nil
+  , no_return
+  , non_neg_integer
+  , none
+  , nonempty_string
+  , number
+  , opaque
+  , pid
+  , port
+  , pos_integer
+  , product
+  , reference
+  , remote
+  , string
+  , term
+  , timeout
+  , tuple
+  , tuple_set
+  , union
+  , var
+  ].
